2be69f2ac8b3e0d55aad012a34e2790ad8c42aeb
Рябко Евгений <jeniaryanko@mail.ru>
2022-12-01 08:30:39 +0300

    FinalVersion

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-var FirstNumber, SecondNumber, Operation;
+var str = "";
 
 window.onload = () => {
-	const arr = [[1, 2, 3, '+'], [4, 5, 6, '-'], [7, 8, 9, '*'], ['C', 0, '/', '=']];
-	let htmlContent = '<tr><td colspan="4"><label for="first"></label><input type="text" id="result" /></td></tr>';
+	const arr = [[1, 2, 3, '+'], [4, 5, 6, '-'], [7, 8, 9, '*'], ['C', 0, "AC", '/'], ['=']];
+	let htmlContent = '<tr><td colspan="4"><label for="first"></label><input type="text" id="result" oninput="CheckLength();" /></td></tr>';
 
 	for (let i = 0; i < arr.length; i++) {
 		const str = arr[i];
 		let tr = ``;
 
-		for (let j = 0; j < str.length; j++)
-			tr += `<td><button type="button" onclick="GetInput('${str[j]}')">${str[j]}</button></td>`;
+		for (let j = 0; j < str.length; j++) {
+			if ("+-/*AC".includes(str[j]))
+				tr += `<td><button type="button" class="ActionButton" onclick="GetInput('${str[j]}')">${str[j]}</button></td>`;
+			else if (str[j] === '=')
+				tr += `<td colspan="4"><button type="button" class="equalButton" onclick="GetInput('${str[j]}')">${str[j]}</button></td>`;
+			else
+				tr += `<td><button type="button" onclick="GetInput('${str[j]}')">${str[j]}</button></td>`;
+		}
 
 		htmlContent += `<tr>${tr}</tr>`;
 	}
@@ -17,45 +23,50 @@ window.onload = () => {
 	document.getElementById('table').innerHTML = htmlContent;
 }
 
-function CalculateResult(First, Operation, Second) {
-	switch (Operation) {
-		case '+':
-			return First + Second;
-		case '-':
-			return First - Second;
-		case '*':
-			return First * Second;
-		case '/':
-			if (Second != 0)
-				return First / Second;
-			return "Dividing by zero!";
+function CheckLength() {
+	if (document.getElementById('result').value.length > 10) {
+		document.getElementById('result').value = document.getElementById('result').value.slice(0, 10);
+		return false;
 	}
+	return true;
 }
 
 function GetInput(val) {
 	result = document.getElementById('result');
 
-	switch (val) {
-		case '=':
-			result.value = eval(document.getElementById('result').value);
-			/* document.getElementById('result').value = CalculateResult(parseInt(FirstNumber), Operation, parseInt(document.getElementById('result').value));
-			FirstNumber = "";
-			Operation = ""; */
-			break;
-		case 'C':
-			result.value = "";
-			break;
-		case '+':
-		case '-':
-		case '*':
-		case '/':
-			if (!"+-/*".includes(result.value[result.value.length - 1]))
+	if (CheckLength()) {
+		switch (val) {
+			case '=':
+				if (str.includes("/0")) {
+					alert("Dividing by zero!");
+					str = "0";
+				}
+				else
+					result.value = eval(str);
+				break;
+			case 'C':
+				result.value = "";
+				str = "";
+				break;
+			case '<=':
+				str = str.slice(0, -1);
+				result.value = result.value.slice(0, -1);
+				break;
+			case '+':
+			case '-':
+			case '*':
+			case '/':
+				result.value = "";
+
+				if (!"+-/*".includes(str[str.length - 1]))
+					str += val;
+				else
+					str = str.slice(0, -1) + val;
+				break;
+			default:
 				result.value += val;
-			else
-				result.value = result.value.slice(0, -1) + val;
-			break;
-		default:
-			result.value += val;
-			break;
+				str += val;
+				break;
+		}
 	}
 }
